7fc1b0c2ae53a9364f6b2726a9fa6f2988d87165
RubFajardo <rubenfajardo68@gmail.com>
2025-07-13 15:04:13 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -1,19 +1,51 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Integer, Table, Column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(120), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    posts: Mapped[List["Post"]] = relationship(back_populates="author")
+    comments: Mapped[List['Comments']] = relationship(back_populates='commenter')
 
 
     def serialize(self):
         return {
             "id": self.id,
+            'username': self.username,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    author: Mapped["User"] = relationship(back_populates="posts")
+    post_comments: Mapped[List["Comments"]] = relationship(back_populates="post_commented")
+    media: Mapped[List["Media"]] = relationship(back_populates="source")
+
+
+
+class Comments(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(nullable=False)
+    commenter_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    commenter: Mapped["User"] = relationship(back_populates="comments")
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+    post_commented: Mapped["Post"] = relationship(back_populates="post_comments")
+
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str] = mapped_column(nullable=False)
+    url: Mapped[str] = mapped_column(nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+    source: Mapped["Post"] = relationship(back_populates="media")
+
+
+    
